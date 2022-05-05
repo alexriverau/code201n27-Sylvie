b1216d76636d22f75ea002f7ea355b23e972a0fa
SYLVIE FISHER <iwalkedwithasylvie@gmail.com>
2022-05-04 20:16:46 -0700

    fixed dialog box

--- js/app.js
+++ js/app.js
@@ -1,47 +1,42 @@
-"use strict";
-
-let answer1 = prompt("How is your day going");
+'use strict';
 
 //console.log(answer1);
 
 if (true) {
-    console.log("In a condition that executes always.");
+  console.log('In a condition that executes always.');
 }
 
 if (false) {
-    console.log("In a condition that executes never!");
+  console.log('In a condition that executes never!');
 }
 
-if (Math.random() >0.5) {
-    console.log("This condition maybe runs!")
+if (Math.random() > 0.5) {
+  console.log('This condition maybe runs!');
 }
 
-answer1 = answer1.toLowerCase();
-if (answer1 == Good!) {
-    console.log("Im glad to hear that!")
-}
+let answer1 = prompt('How is your day going').toLowerCase();
 
-else if (answer1 == "Good!") {
-    console.log("That's excellent!")
-} else if (answer1 == "Just ok" || answer1 == "Bad") {
-    console.log(answer1 + "? I'm sorry to hear that");
-} else if (answer1 == "Just ok") {
-    console.log(Ok is better then bad!);
+
+if (answer1 == 'good') {
+alert('i am glad to hear that');
+} else if (answer1 == 'bad') {
+alert('i am sorry to hear that');
+} else if (answer1 == 'just ok') {
+alert('ok is better then bad');
 } else {
-    console.log(answer1 + Absolutely delightful);
+alert('absolutely delightful');
 }
 
-switch (answer1) {
-    case "Good!":
-    console.log("Thats excellent!");
-    break;
-    case "Just ok":
-        console.log("Thats ok. It happens");
-    break;
-    case "Bad":
-        console.log("I hope it gets better!");
-        break;
-    default:
-        console.log(answer1 + "says 'Keep on killin it!'")
-    }
-
+// switch (answer1) {
+// case 'good':
+//   console.log('that is excellent');
+//   break;
+// case 'just ok':
+//   console.log('thats ok It happens');
+//   break;
+// case 'bad':
+//   console.log('i hope it gets better');
+//   break;
+// default:
+//   console.log(answer1 + 'keep on killin it');
+// }
